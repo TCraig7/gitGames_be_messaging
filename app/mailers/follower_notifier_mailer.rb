71380e6094e4422a78299863c615c281f4c376e1
Tara Craig <tara.craig7@gmail.com>
2019-01-02 20:25:23 -0700

    Add inform method to follower notifier mailer

--- app/mailers/follower_notifier_mailer.rb
+++ app/mailers/follower_notifier_mailer.rb
@@ -1,2 +1,6 @@
 class FollowerNotifierMailer < ApplicationMailer
+  def inform(user, friend_contact)
+    @user = user
+    mail(to: friend_contact, subject: "#{user.username} invites you to GitGames!")
+  end
 end

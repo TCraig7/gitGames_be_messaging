81d800c8259cc0a02ab430c2dcd85500f1e36291
Tara Craig <tara.craig7@gmail.com>
2019-01-03 17:52:16 -0700

    Create model validations and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:username)}
+  end
+
+  xit 'can create a user' do
+
+  end
+end
